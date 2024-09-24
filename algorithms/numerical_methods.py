1eade33381be2cab8193df6f23f51497c194f544
amigo <amigo@hello>
2024-09-24 09:02:10 +0100

    adds finite differences and numerical integration

--- algorithms/numerical_methods.py
+++ algorithms/numerical_methods.py
@@ -27,3 +27,52 @@ def ExplicitEuler(f, times, x0):
         x_new = x_old + (times[i] - times[i-1]) * f(x_old)
         x.append(x_new)
     return x
+
+
+
+#numerical integration
+def traps(a, b, num_steps, func):
+    '''function that takes integration limits, number of points of integration and the function to be integrated and returns
+    the trapzoidal approximation of the integral of the function using equations from MA934 assignment 3.
+    '''
+    h = (b-a)/(num_steps)
+    a1 = (func(a) + func(b))/2
+    asum = 0
+    for i in range(1, num_steps):
+        asum += func(a+i*h)
+    return (a1 + asum) * h 
+
+def simpson(a, b, num_steps, func):
+    '''function that takes integration limits, number of points of integration and the function to be integrated and returns
+    the simpsons 1/3 approximation of the integral of the function using equations from MA934 assignment 3.
+    '''
+    #simpsons third only accuarte for even numbers of step size
+    if num_steps%2 == 0:
+        h = (b-a)/(num_steps)
+        sum1 = 0
+        sum2 = 0
+        for i in range(1, (num_steps//2) +1):
+            sum1 += func(a+((2*i)-1)*h)
+        for i in range(1, num_steps//2):
+            sum2 += func(a + (2*i)*h)
+        return (1/3)*h*(func(a)+func(b)+ 4*sum1 + 2*sum2)
+    return None
+    
+    
+def simpson2(a, b, num_steps, func):
+    '''function that takes integration limits, number of points of integration and the function to be integrated and returns
+    the simpson 3/8 approximation of the integral of the function using equations from MA934 assignment 3.
+    '''
+    #only works for mulitples of three
+    if num_steps%3 == 0:
+        h = (b-a)/(num_steps)
+        sum1 = 0
+        sum2 = 0
+        for i in range(1, int(num_steps)):
+            if i%3 != 0:
+                sum1 += func(a + (i*h))
+        for i in range(1, (int(num_steps/3))):
+            sum2 += func(a + (3*i*h))
+        return (3/8)*h*(func(a)+func(b)+3*sum1 + 2*sum2)
+    return None
+    
